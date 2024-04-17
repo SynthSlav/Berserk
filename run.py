4fe708e287764150255a8e5731217deaa8016da2
SynthSlav <ljupcebozinovski@gmail.com>
2024-04-17 18:13:40 +0200

    modified:   Procfile 	modified:   run.py 	modified:   templates/characters.html

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -49,3 +50,5 @@ if __name__ == "__main__":
         host=os.environ.get("IP", "0.0.0.0"),
         port=os.environ.get("PORT", "3000"),
         debug=True)
+
+
